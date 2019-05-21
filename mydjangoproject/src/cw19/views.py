5f3cbec1f613b24725f3dd27359c29f54a596b21
Ilya Nazaruk <nazaruchok_1995@mail.ru>
2019-05-21 21:49:45 +0300

    add automatic forms

--- mydjangoproject/src/cw19/views.py
+++ mydjangoproject/src/cw19/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cw19.forms import PostForm
 
 # Create your views here.
 
 def comment_add(request):
     if request.method == 'GET':
-        return render(request, 'comment_add.html')
+        context = {'form': PostForm()}
+        return render(request, 'comment_add.html', context)
     elif request.method == 'POST':
-        firstname = request.POST.get('firstname')
-        lastname = request.POST.get('lastname')
-        age = request.POST.get('age')
-        comment = request.POST.get('comment')
-        result = '{} {} {} {}'.format(firstname, lastname, age, comment)
-        print(result)
-        return render(request, 'comment_add.html')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            data = form.data
+            firstname = data.get('firstname')
+            lastname = data.get('lastname')
+            age = data.get('age')
+            comment = data.get('comment')
+            result = '{} {} {} {}'.format(firstname, lastname, age, comment)
+            context = {'form': PostForm()}
+            print(result)
+            return render(request, 'comment_add.html',context)
+        else:
+            errors = form.errors
+            return HttpResponse(errors)
     return HttpResponse('Wrong request method')
