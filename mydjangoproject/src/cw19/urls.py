7e4de3b3752e72154545febb38a4f3efb3a4b598
Ilya Nazaruk <nazaruchok_1995@mail.ru>
2019-05-21 21:12:03 +0300

    add base and name

--- mydjangoproject/src/cw19/urls.py
+++ mydjangoproject/src/cw19/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from cw19.views import comment_add
 
 urlpatterns = [
-    path('add/', comment_add),
+    path('comment_add/', comment_add, name='comment_add'),
 ]
--- mydjangoproject/src/src/settings.py
+++ mydjangoproject/src/src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cw19',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'templates'
+            'templates',
+            'cw19.templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
